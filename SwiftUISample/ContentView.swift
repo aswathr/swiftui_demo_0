c163d920f4976ead2efa9a97c7d79a4c85632cea
Aswath <aswathrce@gmail.com>
2024-02-09 23:19:23 +0530

    Add notch to tab bar

--- SwiftUISample/ContentView.swift
+++ SwiftUISample/ContentView.swift
@@ -10,18 +10,19 @@ import SwiftUI
 struct ContentView1: View {
     var body: some View {
         Text("CV1")
-            .fixedSize()
     }
 }
 
 struct ContentView2: View {
     var body: some View {
         Text("CV2")
-            .fixedSize()
     }
 }
 
 struct MainView: View {
+    
+    @Namespace var namespace
+    @State var currentPage: Int = 0
     var body: some View {
         ScrollViewReader { svProxy in
             VStack {
@@ -29,32 +30,79 @@ struct MainView: View {
                     ScrollView(.horizontal) {
                         HStack {
                             ContentView1()
-                                .frame(width: geoProxy.size.width, height: geoProxy.size.height)
-                                .id(1)
+                                .frame(width: geoProxy.size.width,
+                                       height: geoProxy.size.height)
+                                .id(0)
                             ContentView2()
-                                .frame(width: geoProxy.size.width, height: geoProxy.size.height)
-                                .id(2)
+                                .frame(width: geoProxy.size.width,
+                                       height: geoProxy.size.height)
+                                .id(1)
                         }
                         .frame(maxWidth: .infinity)
                     }
-                    
                     .frame(maxWidth: .infinity)
-                    
                 }
                 
-                HStack {
-                    Button("CV1 btn") {
+                HStack(spacing: 0) {
+                    Button(action: {
                         withAnimation {
+                            currentPage = 0
+                            svProxy.scrollTo(0)
+                        }
+                    }) {
+                        GeometryReader { geoProxy in
+                            Text("CV1")
+                                .foregroundStyle(Color.white)
+                                .frame(maxWidth: .infinity,
+                                       maxHeight: .infinity)
+                                .background(Color.gray)
+                                .overlay {
+                                    if currentPage == 0 {
+                                        Path { path in
+                                            path.move(to: CGPoint(x: (geoProxy.size.width / 2) - 20, y: 0))
+                                            path.addLine(to: CGPoint(x: geoProxy.size.width / 2, y: -20))
+                                            path.addLine(to: CGPoint(x: (geoProxy.size.width / 2) + 20, y: 0))
+                                        }
+                                        .fill(Color.green)
+                                        .matchedGeometryEffect(id: "ID", in: namespace)
+                                    } else {
+                                        EmptyView()
+                                    }
+                                }
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    Button(action: {
+                        withAnimation {
+                            currentPage = 1
                             svProxy.scrollTo(1)
                         }
-                    }
-
-                    Button("CV2 btn") {
-                        withAnimation {
-                            svProxy.scrollTo(2)
+                    }) {
+                        GeometryReader { geoProxy in
+                            Text("CV2")
+                                .foregroundStyle(Color.white)
+                                .frame(maxWidth: .infinity,
+                                       maxHeight: .infinity)
+                                .background(Color.gray)
+                                .overlay {
+                                    if currentPage == 1 {
+                                        Path { path in
+                                            path.move(to: CGPoint(x: (geoProxy.size.width / 2) - 20, y: 0))
+                                            path.addLine(to: CGPoint(x: geoProxy.size.width / 2, y: -20))
+                                            path.addLine(to: CGPoint(x: (geoProxy.size.width / 2) + 20, y: 0))
+                                        }
+                                        .fill(Color.green)
+                                        .matchedGeometryEffect(id: "ID", in: namespace)
+                                    } else {
+                                        EmptyView()
+                                    }
+                                }
                         }
+
                     }
+                    .frame(maxWidth: .infinity)
                 }
+                .frame(height: 50)
             }
         }
     }
